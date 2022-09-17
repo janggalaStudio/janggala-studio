4f568f1fd49543d3d3b1d29e0d394ca0eee15e00
Donny <doniwahyu14@gmail.com>
2022-09-17 18:07:38 +0700

    first commit

---
+++ components/layout/index.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Head from "next/head";
+import Navbar from "../navbar";
+
+export default function Layout(props) {
+  return (
+    <>
+      <Head>
+        <title>{props.title}</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+
+      <div>
+        <Navbar />
+        <div className="container">
+          <main>{props.children}</main>
+        </div>
+      </div>
+    </>
+  );
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const getLayout = Component.getLayout || ((page) => page);
+
+  return getLayout(
+    // <Provider store={store}>
+    <Component {...pageProps} />
+    // </Provider>
+  );
+  // return <Component {...pageProps} />
 }
 
-export default MyApp
+export default MyApp;
