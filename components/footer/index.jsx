ea8b19dbe76a2983454048315978375d2f1647c6
Donny <doniwahyu14@gmail.com>
2022-09-19 02:31:43 +0700

    fix responsive footer

--- components/footer/index.jsx
+++ components/footer/index.jsx
@@ -8,15 +8,15 @@ function Footer() {
         style={{ backgroundColor: "black" }}
       >
         <div className="container">
-          <div className="row">
-            <div className="col mx-3" style={{ color: "white" }}>
+          <div className="row m-0">
+            <div className="col px-3" style={{ color: "white" }}>
               <h3>LOGO</h3>
               <p className="">
                 Janggalastudio, Jalan Mangu, Keyongan, Keyongan, Kabupaten
                 Boyolali, Jawa Tengah
               </p>
             </div>
-            <div className="col mx-3">
+            <div className="col px-3">
               <p style={{ color: "white" }}>MORE INFO</p>
               <div className="row">
                 <p className="border-0" style={{ color: "white" }}>
@@ -34,12 +34,11 @@ function Footer() {
                 </p>
               </div>
             </div>
-            <div className="col max-3">
+            <div className="col px-3">
               <iframe
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3956.0645551238263!2d110.74767561423893!3d-7.458110875576745!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e7a13335200d43f%3A0x49d60cbd12d9bc8b!2sJanggalastudio!5e0!3m2!1sid!2sid!4v1663416555605!5m2!1sid!2sid"
-                width="400"
-                height="200"
                 allowFullScreen=""
+                className="footer-map"
                 loading="lazy"
                 referrerPolicy="no-referrer-when-downgrade"
               ></iframe>
