4e7411936c64c1fba1c85702fd5988cac6ccad79
donny17-bit <doniwahyu14@gmail.com>
2022-09-29 20:50:59 +0700

    add env

--- next.config.js
+++ next.config.js
@@ -4,6 +4,13 @@ const nextConfig = {
   swcMinify: true,
   env: {
     WHATSAPP: process.env.WHATSAPP,
+    FAMILY: process.env.FAMILY,
+    WEDDING: process.env.WEDDING,
+    PREWEDDING: process.env.PREWEDDING,
+    ENGAGEMENT: process.env.ENGAGEMENT,
+    NEW_BORN: process.env.NEW_BORN,
+    GRADUATION: process.env.GRADUATION,
+    GROUP: process.env.GROUP,
   },
 };
 
--- pages/galeri/wedding.jsx
+++ pages/galeri/wedding.jsx
@@ -11,26 +11,14 @@ function Wedding() {
     slide: 1,
   });
 
-  const source = [
-    "/assets/wedding/wedding1.jpg",
-    "/assets/wedding/wedding2.jpg",
-    "/assets/wedding/wedding3.jpg",
-    "/assets/wedding/wedding4.jpg",
-    "/assets/wedding/wedding5.jpg",
-    "/assets/wedding/wedding6.jpg",
-    "/assets/wedding/wedding7.jpg",
-    "/assets/wedding/wedding8.jpg",
-    "/assets/wedding/wedding9.jpg",
-    "/assets/wedding/wedding10.jpg",
-    "/assets/wedding/wedding11.jpg",
-  ];
-
   function openLightboxOnSlide(number) {
     setLightboxController({
       toggler: !lightboxController.toggler,
       slide: number,
     });
   }
+
+  const source = JSON.parse(process.env.WEDDING);
 
   return (
     <>
@@ -47,10 +35,10 @@ function Wedding() {
       <div className="container pb-2" style={{ backgroundColor: "white" }}>
         <div className="row row-cols-4 galeri-row">
           {source.map((item, index) => (
-            <div className="col galeri-col align-self-center px-1">
+            <div className="col galeri-col align-self-center px-1" key={index}>
               <div className="overlay-container">
                 <img
-                  src={`/assets/wedding/wedding${index + 1}.jpg`}
+                  src={item}
                   alt="galeri-wedding"
                   className="galeri-img px-0"
                 />
