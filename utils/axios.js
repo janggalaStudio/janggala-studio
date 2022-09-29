af447da9cb740b428b29f970b2159a8f9bbab70f
donny17-bit <doniwahyu14@gmail.com>
2022-09-29 23:44:12 +0700

    first commit

--- utils/axios.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-const axiosApiIntances = axios.create({
-  baseURL: `https://graph.instagram.com/me/media?fields=id,media_url,media_type&limit=12&access_token=IGQVJXYUF5MUYycGtTLS1XOEs2clRRcXFFRTRpTWdyUktVaHJBakpuVGlEUUgtbGpjdjh2REtyeXRzVVN0eGVndmFad01UMzdvRmJlV09WRDZARZAWdWWXd3ZAS1vQld2eV9FTXp6V0FlQ0Itekg2LWl6cAZDZD`,
-});
-
-export default axiosApiIntances;
